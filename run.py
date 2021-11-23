4710df529188817ffbd47e3faa0b3150936b982c
Cristina Onea <cristinaonea86@gmail.com>
2021-11-23 19:54:13 +0000

    Edit input for answer variable in start_game function

--- run.py
+++ run.py
@@ -1,36 +1,36 @@
 import time 
 
 # This is the start game function that also acts as an intro for the game
-
 def start_game():
     print('''
  +-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
  |T|h|e| |E|n|c|h|a|n|t|e|d| |F|o|r|e|s|t|
  +-+-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
 ''')
+
     
-    # This takes user to the beginning of the game
-    answer = input("Type y to start the game and n to end it.\n").lower().strip()
+    # This takes user to type their name
+    select_name = input("Type your name or username to start the game\n").strip()
+       
+    answer = input(f"Hello {select_name}, welcome to the Enchanted forest!\nType y to start the game and n to end it.\n").strip()
     if answer == "y":
         print("You made the right choice!")
         time.sleep(2)
         game_intro()
         
     elif answer =="n":
-        print("It seems you weren't brave enough to play the game.")
         print("Now you can wake up, it was all a dream")
-        game_over(reason)
-       
+        game_over()
+
     else:
         #Let user know the answer is incorrect
         print("This is an incorrect answer")
-        print("You need to chose yes or no")
+        print("You need to chose yes or no y/n")
         start_game()
 
     
 def game_intro():
-    print("\nWelcome to the Enchanted Forest!")
-    print("The year is 1290, you are on the outskirts of the forest")
+    print("\nThe year is 1290, you are on the outskirts of the forest")
     print("You are picking mushrooms and berries")
     print("You are going further into the forest") 
     print("You don't realize it now but you are straying")
@@ -52,15 +52,18 @@ def game_intro():
         start_game()
 
     else:
-        #Let user know the answer is incorrect and take user to the start of the game
+        """
+        Let user know the answer is incorrect and takes 
+        user to the start of the game
+        """
         print("This is an incorrect answer")
         print("You need to chose yes or no")
         start_game()
 
  
 def two_paths():
-    print("\nYou come at a crossroads")
-    print("There are two paths - you need to chose one of these")
+    print("\nYou come at a crossroads and you see twp paths")
+    print("You need to chose one of these")
     print("Go left to the straigh path across the forest")
     print("This path is lined with trees and it's not well lit")
     print("The second path is on the right")
@@ -87,17 +90,37 @@ def meet_fairy():
     print("She's sitting on a tree log, playing a harph")
     print("She lights up everything around her")
     print("It's getting dark, and she offers to lead you through the forest")
+
     
     answer = input("Which will you chose? Type lantern or fairy to select your choice.\n").lower().strip()
     if answer == "lantern":
-        game_over("You cannot survice alone in the forest")
+        game_over("You cannot survive alone in the forest")
     elif answer == "fairy":
         print("Fantastic choice, the forest fairies will always guide you find your way home")
+        meet_redhead_woman()
     else:
         #Let user know the answer is incorrect
         print("This is an incorrect answer")
         print("You need to chose yes or no")
         start_game()
+
+
+def meet_redhead_woman():
+    print("The fairy brings you to a clearing in the forest")
+    print("You continue your journey through the woods,")
+    print("thinking of the best way to get home to your village")
+    print("But you get hungry and thristy")
+    print("Now you hear water flowing in the distance")
+    print("You walk toweards that direction")
+    print("And you find a river and drink water from it")
+    print("You see a young woman with long red hair")
+    print("She's holding a basket with fruits and meat pies in it")
+    print("You also see a beehive with honey in it")
+    print("You have 2 options, type basket to east the contents of the basket")
+    print("Type honey to eat the honey")
+    
+
+
 
 def game_over():
     print("\n" + "reason")
@@ -111,8 +134,7 @@ def game_over():
         print("You need to chose yes or no")
         start_game()
 
-    start_game()
-
+    
 
 
 start_game()
